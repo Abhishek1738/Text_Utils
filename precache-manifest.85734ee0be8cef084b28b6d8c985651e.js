e62ce7568fd47bc3661050f1e392fb823dc91983
Abhishek <mast.abhishek.gupta@gmail.com>
2023-08-17 01:35:27 +0530

    Updates

---
+++ precache-manifest.85734ee0be8cef084b28b6d8c985651e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3861ff33984e57a18e7f",
+    "url": "/Text_Utils/static/css/main.e0fca2d8.chunk.css"
+  },
+  {
+    "revision": "3861ff33984e57a18e7f",
+    "url": "/Text_Utils/static/js/main.3861ff33.chunk.js"
+  },
+  {
+    "revision": "8c8ed3e6ce86a909c145",
+    "url": "/Text_Utils/static/js/1.8c8ed3e6.chunk.js"
+  },
+  {
+    "revision": "2b8e73089cc5c607c393",
+    "url": "/Text_Utils/static/js/2.2b8e7308.chunk.js"
+  },
+  {
+    "revision": "7449a3e61e3068f4681a",
+    "url": "/Text_Utils/static/js/runtime~main.7449a3e6.js"
+  },
+  {
+    "revision": "de3ad1161f5ecde845580a403fc23936",
+    "url": "/Text_Utils/index.html"
+  }
+];
